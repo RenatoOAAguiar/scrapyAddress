3e7e9dd2a4602c26883424c91c7149523fb5944f
Renato Aguiar <renatooaa@outlook.com>
2017-06-21 10:34:16 -0300

    Commit inicial

---
+++ crawler/spider/scrapyAddress.py
@@ -0,0 +1,31 @@
+from scrapy.spiders import Spider
+from scrapy.selector import  Selector
+from searchResult import searchResultPages
+from searchEngine import SearchEngineResult
+
+class SpiderAddress(Spider):
+    name = 'SpiderAddress'
+    start_urls = []
+    allowed_domains = ['google.com.br', 'google.com']
+    keyword = None
+    searchEngine = None
+    selector = None
+
+    def __init__(self, keyword, results,  *args, **kwargs):
+        super(SpiderAddress, self).__init__(*args, **kwargs)
+        self.keyword = keyword.lower()
+        self.searchEngine = 'google'
+        self.selector = SearchEngineResult[self.searchEngine]
+        pageUrls = searchResultPages(keyword, self.searchEngine , 1)
+        for url in pageUrls:
+            print(url)
+            self.start_urls.append(url)
+
+    def parse(self, response):
+        for title in response.css('body'):
+            yield {
+            'endereco': title.css('body ::text').extract()
+            }
+
+        for next_page in response.css('div > a'):
+            yield response.follow(next_page, self.parse)
---
+++ crawler/spider/searchEngine.py
@@ -0,0 +1,8 @@
+SearchEngines = {
+    'google': 'https://www.google.com.br/search?q={0}&start={1}'
+}
+
+
+SearchEngineResult= {
+    'google': '//h3/a/@href'
+}
